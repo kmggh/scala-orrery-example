eb1f5554988467d386774d11e94c4ede8bba054b
kmggh <kenguyton@ezkbcbrdyyrbwpqmeemzvaunr.com>
2013-01-30 05:21:40 -0500

    Reorganized the run object into multiple methods.

--- run_orrery.scala
+++ run_orrery.scala
@@ -15,18 +15,19 @@ import orrery._
  * The count and step size can be specified on the command line.
  */
 object RunOrrery {
-  def main(args: Array[String]) {
-    var delta_t = 0.5
-    var step_count = 5
-    val orrery = SmallOrrery
+  /**
+   * Parse the args to get the step_count and delta_t time step.
+   */
+  var step_count = 5
+  var delta_t = 0.5
 
+  def parseArgs(args: Array[String]) = {
     if (args.length == 1 && args(0) == "--help") {
       println("scala RunOrrery [[delta_t] count]")
       System.exit(0)
     }
 
     if (args.length == 1) {
-      delta_t = 0.5
       step_count = args(0).toInt
       println("Set step_count to " + step_count)
     }
@@ -34,17 +35,35 @@ object RunOrrery {
       delta_t = args(0).toDouble
       step_count = args(1).toInt
     }
+  }
 
+  /*
+   * Print the intro.
+   */
+  def printIntro  = {
     println("RunOrrery using the orrery package.")
-
     println("Stepping the orrery " + step_count +
             " times after the initial position.")
     println("With a time step increment of " + delta_t + " year(s).\n")
+  }
 
-    println(orrery)
+  /*
+   * Print the initial position of the orrery.  Then step through each
+   * delta_t for step_count times and print out the positions.
+   */
+  def runAndPrintTheOrrery  = {
+    val the_orrery = SmallOrrery
+
+    println(the_orrery)
     for (i <- 1 to step_count) {
-      orrery.step(delta_t)
-      println(orrery)
+      the_orrery.step(delta_t)
+      println(the_orrery)
     }
+  }
+
+  def main(args: Array[String]) {
+    parseArgs(args)
+    printIntro
+    runAndPrintTheOrrery
   }
 }
